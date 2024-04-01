1b0013943205dc8f3be6ca5420af94d25c904773
Windyphan <thandongdatviettatca@gmail.com>
2024-04-01 13:17:52 +0700

    Add entity answer

---
+++ src/main/java/com/example/demo/entity/Answer.java
@@ -0,0 +1,55 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "answer")
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "is_correct")
+    private boolean correct;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
