da6e625ff5be7005807af2aff6a5e09e2efbb623
Windyphan <thandongdatviettatca@gmail.com>
2024-04-10 17:53:12 +0700

    Add try catch and change return from entity to DTO

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.TestSubmission;
+import com.example.demo.DTOs.TestSubmissionDTO;
 import com.example.demo.service.AuthenticationService;
 import com.example.demo.service.JWTUtil;
 import com.example.demo.service.StudentService;
 import com.example.demo.service.TestSubmissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -30,26 +32,32 @@ public class StudentController {
     }
 
     // API endpoint to allow students to take tests
-    @PostMapping("/{studentId}/test/{testId}/submit")
+    @PostMapping("/test/submit")
     @PreAuthorize("hasRole(T(UserRole).STUDENT)")
-    public TestSubmission submitTest(
+    public TestSubmissionDTO submitTest(
             @RequestHeader("Authorization") String token,
-            @RequestBody Long studentId,
-            @RequestBody TestSubmission submission
+            @RequestBody TestSubmissionDTO submission
     ) {
-        authenticationService.authenticateToken(token);
-        submission.setId(studentId);
-        testSubmissionService.submitTest(submission);
-        return submission;
+        try{
+            authenticationService.authenticateToken(token);
+            testSubmissionService.submitTest(submission);
+            return submission;
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage());
+        }
     }
 
     @GetMapping("/{studentId}/test-results")
     @PreAuthorize("hasRole(T(UserRole).STUDENT)")
-    public List<TestSubmission> getTestResults(
+    public List<TestSubmissionDTO> getTestResults(
             @RequestHeader("Authorization") String token,
             @PathVariable Long studentId) {
-        authenticationService.authenticateToken(token);
-        List<TestSubmission> testResults = testSubmissionService.getTestResults(studentId);
-        return testResults;
+        try{
+            authenticationService.authenticateToken(token);
+            List<TestSubmissionDTO> testResults = testSubmissionService.getTestResults(studentId);
+            return testResults;
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,ex.getMessage());
+        }
     }
 }
