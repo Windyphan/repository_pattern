07a561a8aeb20446404548d51e3a7cece1136e67
Windyphan <thandongdatviettatca@gmail.com>
2024-04-10 18:17:23 +0700

    Add more logic for question id and answer content

--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -59,46 +59,50 @@ public class TestService {
 
     public void updateTest(Long id, Test updatedTest) {
         Test existingTest = testRepository.findByTestId(id);
-        if (existingTest != null) {
-            if (updatedTest.getName() != null || !updatedTest.getName().isEmpty()) {
-                existingTest.setName(updatedTest.getName());
-            }
-            if(updatedTest.getDescription() != null || !updatedTest.getDescription().isEmpty()) {
-                existingTest.setDescription(updatedTest.getDescription());
-            }
-            if (updatedTest.getQuestions() != null && !updatedTest.getQuestions().isEmpty()) {
-                for (Question question : updatedTest.getQuestions()) {
-                    // Update only if there is a value
-                    existingTest.getQuestions().stream()
-                            .filter(existingQuestion -> existingQuestion.getId().equals(question.getId())) // Assuming question ID is unique
-                            .findFirst()
-                            .ifPresent(existingQuestion -> {
-                                if (question.getContent() != null && !question.getContent().isEmpty()) {
-                                    existingQuestion.setContent(question.getContent());
-                                }
-                                if (question.getAnswers() != null && !question.getAnswers().isEmpty()) {
-                                    for (Answer answer : question.getAnswers()) {
-                                        existingQuestion.getAnswers().stream()
-                                                .filter(existingAnswer -> existingAnswer.getId().equals(answer.getId())) // Assuming question ID is unique
-                                                .findFirst()
-                                                .ifPresent(existingAnswer -> {
-                                                    if (answer.getContent() != null && !answer.getContent().isEmpty()) {
-                                                        // Update only if there is a value
-                                                        existingAnswer.setContent(answer.getContent());
-                                                    }
-                                                });
-                                    }
-                                }
-
-                            });
-
-                }
-            }
-
-            testRepository.save(existingTest);
-        } else {
+        if (existingTest == null) {
             throw new IllegalArgumentException("Test not existing");
         }
+        if (updatedTest.getName() != null || !updatedTest.getName().isEmpty()) {
+            existingTest.setName(updatedTest.getName());
+        }
+        if(updatedTest.getDescription() != null || !updatedTest.getDescription().isEmpty()) {
+            existingTest.setDescription(updatedTest.getDescription());
+        }
+        if (updatedTest.getQuestions() != null && !updatedTest.getQuestions().isEmpty()) {
+            for (Question question : updatedTest.getQuestions()) {
+                // Update only if there is a value
+                if (question.getId() == null || updatedTest.getQuestions().isEmpty()) {
+                    throw new IllegalArgumentException("Question id is required");
+                }
+                existingTest.getQuestions().stream()
+                        .filter(existingQuestion -> existingQuestion.getId().equals(question.getId())) // Assuming question ID is unique
+                        .findFirst()
+                        .ifPresent(existingQuestion -> {
+                            if (question.getContent() != null && !question.getContent().isEmpty()) {
+                                existingQuestion.setContent(question.getContent());
+                            }
+                            if (question.getAnswers() != null && !question.getAnswers().isEmpty()) {
+                                for (Answer answer : question.getAnswers()) {
+                                    if (answer.getId() == null || updatedTest.getQuestions().isEmpty()) {
+                                        throw new IllegalArgumentException("Answer id is required");
+                                    }
+                                    existingQuestion.getAnswers().stream()
+                                            .filter(existingAnswer -> existingAnswer.getId().equals(answer.getId())) // Assuming question ID is unique
+                                            .findFirst()
+                                            .ifPresent(existingAnswer -> {
+                                                if (answer.getContent() != null && !answer.getContent().isEmpty()) {
+                                                    // Update only if there is a value
+                                                    existingAnswer.setContent(answer.getContent());
+                                                }
+                                            });
+                                }
+                            }
+
+                        });
+            }
+        }
+
+        testRepository.save(existingTest);
     }
 
     public void deleteTest(Long id) {
