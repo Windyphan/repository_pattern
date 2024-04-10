94cee0d566f1d638056513983936c3f95adcb6e2
Windyphan <thandongdatviettatca@gmail.com>
2024-04-10 17:44:36 +0700

    Add new query to fetch data

--- src/main/java/com/example/demo/repository/TestRepository.java
+++ src/main/java/com/example/demo/repository/TestRepository.java
@@ -2,9 +2,13 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestRepository extends JpaRepository<Test, Long> {
     Test findByTestId(Long id);
+    @Query("SELECT DISTINCT t FROM Test t JOIN FETCH t.questions q JOIN FETCH q.answers a WHERE t.testId = :testId")
+    Test findTestChildEntitiesByTestId(@Param("testId") Long testId);
 }
