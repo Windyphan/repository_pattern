03dff8dc22edbfb74835d37ebd9f0644e8bfa11e
Windyphan <thandongdatviettatca@gmail.com>
2024-04-10 17:51:44 +0700

    Remove redundant code

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 
 import com.example.demo.entity.TestSubmission;
-import com.example.demo.repository.StudentRepository;
 import com.example.demo.repository.TestSubmissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,17 +10,8 @@ import java.util.List;
 
 @Service
 public class StudentService {
-
-    @Autowired
-    private StudentRepository studentRepository;
-
     @Autowired
     private TestSubmissionRepository testSubmissionRepository;
-
-    @Autowired
-    private TestService testService;
-
-
 
     public List<TestSubmission> getTestResultsForStudent(Long studentId) {
         return testSubmissionRepository.findByStudentId(studentId);
